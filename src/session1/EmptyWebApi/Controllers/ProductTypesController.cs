82903cfd97ad5b15ebe59af4c64dc2d08bfe098b
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-18 20:16:43 +0200

    added product types controller

---
+++ src/session1/EmptyWebApi/Controllers/ProductTypesController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EmptyWebApi.Controllers
+{
+    public class ProductTypesController
+    {
+        /*
+         * create ProductTypes controller
+         * create model for ProductType(Id,Name)
+         * install entity framework
+         * implement entity framework DbContext
+         * implement CRUD operation
+         */
+    }
+}
