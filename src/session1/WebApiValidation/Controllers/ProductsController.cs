630922d796241c3b2c77f403201ea0fca3f437ae
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-20 19:12:01 +0200

    added routing

--- src/session1/WebApiValidation/Controllers/ProductsController.cs
+++ src/session1/WebApiValidation/Controllers/ProductsController.cs
@@ -3,32 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using WebApiValidation.Models;
+using WebApiValidation.Models.Requests;
 
 namespace WebApiValidation.Controllers
 {
+    [RoutePrefix("/products")]
     public class ProductsController: ApiController
     {
+        private static List<Product> _products = new List<Product>();
+
+        [HttpGet]
+        [Route("")]
         public IHttpActionResult GetAll()
         {
 
         }
 
-        public IHttpActionResult GetById()
+        [HttpGet]
+        [Route("{id:min(1)}")]
+        public IHttpActionResult GetById(int id)
+        {
+
+
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Create(CreateProductRequest product)
         {
 
         }
 
-        public IHttpActionResult Create()
+        [HttpPut]
+        [Route("{id:min(1)}")]
+        public IHttpActionResult Update(int id, UpdateProductRequest product)
         {
 
         }
 
-        public IHttpActionResult Update()
-        {
-
-        }
-
-        public IHttpActionResult Delete()
+        [HttpDelete]
+        [Route("{id:min(1)}")]
+        public IHttpActionResult Delete(int id)
         {
 
         }
