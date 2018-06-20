14b982b27c2124e64e1dbdbcfaee6da5102b1201
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-20 18:55:34 +0200

    added WebApiValidation project

---
+++ src/session1/WebApiValidation/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiValidation.Controllers
+{
+    public class ProductsController: ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+
+        }
+
+        public IHttpActionResult GetById()
+        {
+
+        }
+
+        public IHttpActionResult Create()
+        {
+
+        }
+
+        public IHttpActionResult Update()
+        {
+
+        }
+
+        public IHttpActionResult Delete()
+        {
+
+        }
+    }
+}
