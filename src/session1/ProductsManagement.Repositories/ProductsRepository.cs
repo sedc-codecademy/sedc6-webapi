7c5d5220b28d3e86bd9ed370f942f002dd2df6c5
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-22 19:26:55 +0200

    added httpclient

--- src/session1/ProductsManagement.Repositories/ProductsRepository.cs
+++ src/session1/ProductsManagement.Repositories/ProductsRepository.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using ProductsManagement.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace ProductsManagement.Repositories
 {
     public class ProductsRepository
     {
+        private string baseUrl = "http://10.10.86.140:8888";
+
+        public List<GetProductResponse> GetAllProducts()
+        {
+            var client = new HttpClient();
+
+
+
+        }
     }
 }
