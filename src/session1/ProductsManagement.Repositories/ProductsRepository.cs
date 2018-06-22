8b4c5ebb4e40b3d84bfb7cfe28b2a637b499433a
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-22 19:42:36 +0200

    added getProductsMethod

--- src/session1/ProductsManagement.Repositories/ProductsRepository.cs
+++ src/session1/ProductsManagement.Repositories/ProductsRepository.cs
@@ -1,7 +1,9 @@
-﻿using ProductsManagement.Models.Responses;
+﻿using Newtonsoft.Json;
+using ProductsManagement.Models.Responses;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ProductsManagement.Repositories
 {
@@ -9,12 +11,19 @@ namespace ProductsManagement.Repositories
     {
         private string baseUrl = "http://10.10.86.140:8888";
 
-        public List<GetProductResponse> GetAllProducts()
+        public async Task<List<GetProductResponse>> GetAllProductsAsync()
         {
-            var client = new HttpClient();
+var client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync($"{baseUrl}/products");
 
+if (!response.IsSuccessStatusCode)
+    return null;
 
+var content = await response.Content.ReadAsStringAsync();
 
+var data = JsonConvert.DeserializeObject<List<GetProductResponse>>(content);
+
+            return data;
         }
     }
 }
