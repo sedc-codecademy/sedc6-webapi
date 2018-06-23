25e6c64b7d794d7c79d8e56413bcaaf4c294c3a1
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-06-23 14:29:20 +0200

    added dotnet core web api

---
+++ src/session1/WebApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //foreach (var arg in args)
+            //{
+            //    Console.WriteLine($"arg value: {arg}");
+            //}
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/session1/WebApplication1/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Use(async (context, next) =>
+            {
+                try
+                {
+                    await next();
+                }
+                catch (Exception ex)
+                {
+                   await  context.Response.WriteAsync(ex.Message);
+                }
+            });
+
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value=="/products")
+                {
+                    throw new Exception("no products available");
+                }
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
